87750a56f006564ac04a9065245a9164859d6851
edg_alves <edgar.costa@dunelm.com>
2020-04-15 18:59:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Avatar } from "./Components/Avatar";
+import { hot } from "react-hot-loader";
+
+const App = () => {
+  return <Avatar></Avatar>;
+};
+
+export default hot(module)(App);
